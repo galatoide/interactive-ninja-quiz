178613b93d2f8e53150f3e8612eaed09f846ae20
Wilson Cruz <galatoide@gmail.com>
2020-08-05 14:32:43 +0100

    animate the score

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ form.addEventListener('submit', e => {
     })
 
     // show result
+    scrollTo(0,0)
     result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
-})
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 80)
+});
